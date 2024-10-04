24853735d7e4db7f02b11127473e512ce923b0c2
marccosabino <marco.sabinosr@gmail.com>
2024-10-04 17:51:08 -0300

    update

--- meuPrimeiroPrograma.js
+++ meuPrimeiroPrograma.js
@@ -1,3 +1,12 @@
-console.log("Meu primeiro programa");
+console.log("Meu Primeiro programa: trabalhando com variáveis");
 
-const idade = 20;
+const idade = 26;
+
+console.log("idade");
+console.log(idade);
+console.log(idade+2);
+console.log(idade-2);
+console.log(idade/2);
+
+const idadeSomada = idade+2;
+console.log(idadeSomada);
